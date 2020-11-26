d50de4ec76001e9d1faf64f76544095548141b94
Carlos Baeza <carlos@spore.cl>
2020-11-25 21:24:57 -0300

    refactor and prepare production ready environment

--- contracts/SporeTokenMock.sol
+++ contracts/SporeTokenMock.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SporeToken is ERC20 {
-    constructor() public ERC20("Spore Rinkeby", "rinkSPORE") {
+    constructor() public ERC20("Spore Rinkebyy", "rinkkSPORE") {
         _mint(msg.sender, 100000000000000000000000000);
     }
 }
